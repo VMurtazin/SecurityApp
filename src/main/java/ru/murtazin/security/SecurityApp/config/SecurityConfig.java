b39f20e9eb913f3038c0329abbf8f0e589145043
VMurtazin <obdorsk@gmail.com>
2023-03-26 20:39:43 +0500

    add_csrf

--- src/main/java/ru/murtazin/security/SecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/murtazin/security/SecurityApp/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.murtazin.security.SecurityApp.services.PersonDetailsService;
@@ -23,17 +24,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) {
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/auth/login","/auth/registration", "/error").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/auth/login")
+            .loginProcessingUrl("/process_login")
+            .defaultSuccessUrl("/hello",true)
+            .failureUrl("/process_login?error")
+            .and()
+            .logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/auth/login");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+        auth.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
